2a1b8b5f70d28b962fbf53a6c700487782da3aa8
wangluyuan <wangluyuan@bytedance.com>
2020-03-07 11:11:49 +0800

    feat: 增加备案号

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,8 @@ class Footer extends React.Component {
         return(
             <div className="footer">
                 <span className="footer-text">www.wangluyuan.cc</span>
-                <span className="footer-text">© 2019 Luyuan Wang</span>
+                <span className="footer-text">© 2020 Luyuan Wang</span>
+                <a href="http://www.beian.miit.gov.cn" className="footer-link-gov">京ICP备17051145号</a>
             </div>
         )
     }
