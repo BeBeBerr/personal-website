c37daccd922eef0f9f8f06ab99976e62d3c012af
Luyuan Wang <e@wangluyuan.cc>
2020-04-26 23:54:14 +0800

    fix: use two options for maps

--- src/components/map.js
+++ src/components/map.js
@@ -1,93 +1,6 @@
 import React from 'react';
 import '../styles/map.css';
 
-var option = {
-    backgroundColor: '#404a59',
-    title: {
-        text: 'Visitor Map',
-        left: 'right',
-        textStyle: {
-            color: '#fff'
-        }
-    },
-    tooltip: {
-        trigger: 'item'
-    },
-    legend: {
-        orient: 'vertical',
-        top: 'bottom',
-        left: 'right',
-        data:['pm2.5'],
-        textStyle: {
-            color: '#fff'
-        }
-    },
-    toolbox: {
-        show: true,
-        orient: 'vertical',
-        left: 'left',
-        top: 'top',
-        feature: {
-            restore: {},
-            saveAsImage: {}
-        }
-    },
-    visualMap: {
-        min: 0,
-        max: 30,
-        splitNumber: 5,
-        color: ['#d94e5d','#eac736','#50a3ba'],
-        textStyle: {
-            color: '#fff'
-        }
-    },
-    geo: {
-        map: 'world-map',
-        label: {
-            emphasis: {
-                show: false
-            }
-        },
-        itemStyle: {
-            normal: {
-                areaColor: '#323c48',
-                borderColor: '#111'
-            },
-            emphasis: {
-                areaColor: '#2a333d'
-            }
-        },
-        roam: true,
-        scaleLimit: { //滚轮缩放的极限控制
-            min: 1,
-            max: 4
-        },
-        zoom: 1,
-    },
-    series: [
-        {
-            name: 'Count',
-            type: 'scatter',
-            coordinateSystem: 'geo',
-            symbolSize: 12,
-            label: {
-                normal: {
-                    show: false
-                },
-                emphasis: {
-                    show: false
-                }
-            },
-            itemStyle: {
-                emphasis: {
-                    borderColor: '#fff',
-                    borderWidth: 1
-                }
-            },
-        }
-    ]
-};
-
 class VisitorMap extends React.Component {
 
     componentDidMount() {
@@ -101,6 +14,93 @@ class VisitorMap extends React.Component {
 
         fetch('http://api.wangluyuan.cc/visit/result').then(response => response.json()).then((visitor_records) => {
             fetch('https://raw.githubusercontent.com/apache/incubator-echarts/master/map/json/world.json').then(response => response.json()).then((data) => {
+
+                var option = {
+                    backgroundColor: '#404a59',
+                    title: {
+                        text: 'Visitor Map',
+                        left: 'right',
+                        textStyle: {
+                            color: '#fff'
+                        }
+                    },
+                    tooltip: {
+                        trigger: 'item'
+                    },
+                    legend: {
+                        orient: 'vertical',
+                        top: 'bottom',
+                        left: 'right',
+                        data:['pm2.5'],
+                        textStyle: {
+                            color: '#fff'
+                        }
+                    },
+                    toolbox: {
+                        show: true,
+                        orient: 'vertical',
+                        left: 'left',
+                        top: 'top',
+                        feature: {
+                            restore: {},
+                            saveAsImage: {}
+                        }
+                    },
+                    visualMap: {
+                        min: 0,
+                        max: 30,
+                        splitNumber: 5,
+                        color: ['#d94e5d','#eac736','#50a3ba'],
+                        textStyle: {
+                            color: '#fff'
+                        }
+                    },
+                    geo: {
+                        map: 'world-map',
+                        label: {
+                            emphasis: {
+                                show: false
+                            }
+                        },
+                        itemStyle: {
+                            normal: {
+                                areaColor: '#323c48',
+                                borderColor: '#111'
+                            },
+                            emphasis: {
+                                areaColor: '#2a333d'
+                            }
+                        },
+                        roam: true,
+                        scaleLimit: { //滚轮缩放的极限控制
+                            min: 1,
+                            max: 4
+                        },
+                        zoom: 1,
+                    },
+                    series: [
+                        {
+                            name: 'Count',
+                            type: 'scatter',
+                            coordinateSystem: 'geo',
+                            symbolSize: 12,
+                            label: {
+                                normal: {
+                                    show: false
+                                },
+                                emphasis: {
+                                    show: false
+                                }
+                            },
+                            itemStyle: {
+                                emphasis: {
+                                    borderColor: '#fff',
+                                    borderWidth: 1
+                                }
+                            },
+                        }
+                    ]
+                };
             
                 var worldMap = echarts.init(document.getElementById('global-map'));
                 echarts.registerMap('world-map', data);
@@ -111,6 +111,93 @@ class VisitorMap extends React.Component {
             });
 
             fetch('https://raw.githubusercontent.com/apache/incubator-echarts/master/map/json/china.json').then(response => response.json()).then((data) => {
+
+                var option = {
+                    backgroundColor: '#404a59',
+                    title: {
+                        text: 'Visitor Map',
+                        left: 'right',
+                        textStyle: {
+                            color: '#fff'
+                        }
+                    },
+                    tooltip: {
+                        trigger: 'item'
+                    },
+                    legend: {
+                        orient: 'vertical',
+                        top: 'bottom',
+                        left: 'right',
+                        data:['pm2.5'],
+                        textStyle: {
+                            color: '#fff'
+                        }
+                    },
+                    toolbox: {
+                        show: true,
+                        orient: 'vertical',
+                        left: 'left',
+                        top: 'top',
+                        feature: {
+                            restore: {},
+                            saveAsImage: {}
+                        }
+                    },
+                    visualMap: {
+                        min: 0,
+                        max: 30,
+                        splitNumber: 5,
+                        color: ['#d94e5d','#eac736','#50a3ba'],
+                        textStyle: {
+                            color: '#fff'
+                        }
+                    },
+                    geo: {
+                        map: 'world-map',
+                        label: {
+                            emphasis: {
+                                show: false
+                            }
+                        },
+                        itemStyle: {
+                            normal: {
+                                areaColor: '#323c48',
+                                borderColor: '#111'
+                            },
+                            emphasis: {
+                                areaColor: '#2a333d'
+                            }
+                        },
+                        roam: true,
+                        scaleLimit: { //滚轮缩放的极限控制
+                            min: 1,
+                            max: 4
+                        },
+                        zoom: 1,
+                    },
+                    series: [
+                        {
+                            name: 'Count',
+                            type: 'scatter',
+                            coordinateSystem: 'geo',
+                            symbolSize: 12,
+                            label: {
+                                normal: {
+                                    show: false
+                                },
+                                emphasis: {
+                                    show: false
+                                }
+                            },
+                            itemStyle: {
+                                emphasis: {
+                                    borderColor: '#fff',
+                                    borderWidth: 1
+                                }
+                            },
+                        }
+                    ]
+                };
 
                 var chinaMap = echarts.init(document.getElementById('china-map'));
                 echarts.registerMap('world-map', data);
